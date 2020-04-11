ba8254999de5be69609ea5feb485919a0160f8ab
NeboM <nebbo.o.97@gmail.com>
2020-04-11 16:28:17 +0200

    taxReturn method finished

--- SI_lab1_gr3_173238/Item.java
+++ SI_lab1_gr3_173238/Item.java
@@ -7,9 +7,37 @@ class Item {
 	char karakter;
 
 	//TODO constructor
+	Item(int id, String name, double price, char karakter){
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.karakter = karakter;
+	}
 
 	//TODO setters and getters
-
+	public int getId(){
+		return this.id;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public char getKarakter(){
+		return this.karakter;
+	}
+	public void setId(int id){
+		this.id = id;;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public void setKarakter(char c){
+		this.karakter = c;
+	}
+		
+	double getTaxReturn(){
+		return this.price * 0.15;
+	}
+	
 	double taxReturn () {
 		//TODO
 	}
